fb920efe21d590fbeb9df5888fef4eb5e00fa5bf
Richard Waid <richard@iopen.net>
2004-11-28 00:47:33 +0000

    Added a user_id generator
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFCore/trunk%40461

--- XWFUtils.py
+++ XWFUtils.py
@@ -116,3 +116,25 @@ def createBatch(result_set, b_start, b_size):
         b_end = b_start + b_size
     
     return (b_start, b_end, b_size, result_size, result_set) 
+
+def generate_user_id(user_id='', first_name='', last_name='', email=''):
+    if first_name and last_name:
+        yield first_name+last_name[0]
+        yield last_name+first_name[0]
+        yield first_name+'.'+last_name
+        yield last_name+'.'+first_name
+    if email:
+        try:
+            yield email.split('@')[0]
+        except:
+            pass
+    if user_id:
+        i = 1
+        while 1:
+            yield user_id+'%s' % i
+            i += 1
+    if first_name and last_name:
+        i = 1
+        while 1:
+            yield first_name+'.'+last_name+'%s' % i
+            i += 1
