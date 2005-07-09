0668a0c42e774d770716aedd1f5afebef097e247
Richard Waid <richard@iopen.net>
2005-07-09 11:20:08 +0000

    Major alterations to the XWFCatalog, enabling addition from the ZMI, as well as alterations to the way users and roles are calculated.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFCore/trunk%40595

--- XWFCatalog.py
+++ XWFCatalog.py
@@ -17,12 +17,49 @@
 # You MUST follow the rules in http://iopen.net/STYLE before checking in code
 # to the trunk. Code which does not follow the rules will be rejected.
 #
-from Products.CMFCore.CatalogTool import IndexableObjectWrapper
+from types import StringType
+
 from AccessControl import getSecurityManager
 from AccessControl import ClassSecurityInfo
 from Products.ZCatalog.ZCatalog import ZCatalog
 from Acquisition import aq_base
-from types import StringType
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+
+class Record:
+    pass
+
+class XWFIndexableObjectWrapper:
+    # Primarily borrowed from CMFCore
+    def __init__(self, vars, ob):
+        self.__vars = vars
+        self.__ob = ob
+
+    def __getattr__(self, name):
+        vars = self.__vars
+        if vars.has_key(name):
+            return vars[name]
+        return getattr(self.__ob, name)
+
+    def allowedRolesAndUsers(self):
+        """
+        Return a list of roles and users with View permission.
+        Used by PortalCatalog to filter out items you're not allowed to see.
+        """
+        ob = self.__ob
+        return self.acl_users._allowedRolesAndUsers(ob)
+
+        allowed = {}
+        for r in rolesForPermissionOn(View, ob):
+            allowed[r] = 1
+        localroles = _mergedLocalRoles(ob)
+        for user, roles in localroles.items():
+            for role in roles:
+                if allowed.has_key(role):
+                    allowed['user:' + user] = 1
+        if allowed.has_key('Owner'):
+            del allowed['Owner']
+        return list(allowed.keys())
+
 
 class XWFCatalog(ZCatalog):
     """ A ZCatalog that adds additional filters to queries, primarily
@@ -31,26 +68,39 @@ class XWFCatalog(ZCatalog):
     """
     security = ClassSecurityInfo()
     
-    id = 'xwf catalog'
     meta_type = 'XWF Catalog'
     
-    def __init__(self):
+    def __init__(self, id):
         ZCatalog.__init__(self, self.getId())
         self.addIndex('allowedRolesAndUsers', 'KeywordIndex')
         
-    def catalog_object(self, object, uid=None, idxs=[]):         
-        # Wraps the object with workflow and accessibility
-        # information just before cataloging.
+    def __url(self, ob):
+        return '/'.join(ob.getPhysicalPath())
+
+    def catalog_object(self, object, uid=None, idxs=[], update_metadata=1):         
         vars = {}
-        w = IndexableObjectWrapper(vars, object)
-        ZCatalog.catalog_object(self, w, uid, idxs)
+        w = XWFIndexableObjectWrapper(vars, object)
+        ZCatalog.catalog_object(self, w, uid, idxs, update_metadata)
 
-    def _listAllowedRolesAndUsers( self, user ):
-        result = list( user.getRolesInContext(self) )
-        result.append( 'Anonymous' )
-        result.append( 'user:%s' % user.getId() )
+    def _listAllowedRolesAndUsers(self, user):
+        result = list(user.getRoles())
+        if not result:
+            result.append('Anonymous')
+        else:
+            result.append('user:%s' % user.getUserName())
+        # deal with groups
+        getGroups = getattr(user, 'getGroups', None)
+        if getGroups is not None:
+            groups = user.getGroups()
+            if 'Anonymous' in result:
+                groups = groups + ('role:Anonymous',)
+            if 'Authenticated' in result:
+                groups = groups + ('role:Authenticated',)
+            for group in groups:
+                result.append('group:%s' % group)
+        # end groups
         return result
-
+    
     # searchResults has inherited security assertions.
     # searchResults and unrestrictedSearchResults are 'borrowed' 
     # from CMFCore
@@ -64,13 +114,14 @@ class XWFCatalog(ZCatalog):
         # we replace, rather than append the 'allowedRolesAndUsers' index,
         # anything else would be a really big security loophole
         kw[ 'allowedRolesAndUsers' ] = self._listAllowedRolesAndUsers( user )
+        
         return ZCatalog.searchResults(self, REQUEST, **kw)
- 
+    
     __call__ = searchResults
 
     security.declarePrivate('unrestrictedSearchResults')
     def unrestrictedSearchResults(self, REQUEST=None, **kw):
-        """Calls ZCatalog.searchResults directly without restrictions.
+        """ Calls ZCatalog.searchResults directly without restrictions.
 
         This method returns every also not yet effective and already expired
         objects regardless of the roles the caller has.
@@ -80,6 +131,114 @@ class XWFCatalog(ZCatalog):
 
         If you're in doubt if you should use this method or
         'searchResults' use the latter.
+        
         """
-        return ZCatalog.searchResults(self, REQUEST, **kw)
+        return ZCatalog.searchResults(self, REQUEST, **kw)    
 
+    security.declarePrivate('indexObject')
+    def indexObject(self, object):
+        """ Add to catalog.
+        
+        """
+        url = self.__url(object)
+        self.catalog_object(object, url)
+
+    security.declarePrivate('unindexObject')
+    def unindexObject(self, object):
+        """ Remove from catalog.
+        
+        """
+        url = self.__url(object)
+        self.uncatalog_object(url)
+
+    security.declarePrivate('reindexObject')
+    def reindexObject(self, object, idxs=[], update_metadata=1):
+        """ Update catalog after object data has changed.
+        The optional idxs argument is a list of specific indexes
+        to update (all of them by default).
+        
+        """
+        url = self.__url(object)
+        if idxs != []:
+            # Filter out invalid indexes.
+            valid_indexes = self._catalog.indexes.keys()
+            idxs = [i for i in idxs if i in valid_indexes]
+        self.catalog_object(object, url, idxs, update_metadata)
+
+    def manage_afterAdd(self, item, container):
+        ZCatalog.manage_afterAdd(self, item, container)
+        
+        if not hasattr(item.aq_explicit, 'Lexicon'):
+            wordsplitter = Record()
+            wordsplitter.group = 'Word Splitter'
+            wordsplitter.name = 'HTML aware splitter'
+            
+            casenormalizer = Record()
+            casenormalizer.group = 'Case Normalizer'
+            casenormalizer.name = 'Case Normalizer'
+            
+            stopwords = Record()
+            stopwords.group = 'Stop Words'
+            stopwords.name = 'Remove listed and single char words'
+            
+            item.manage_addProduct['ZCTextIndex'].manage_addLexicon(
+                                                 'Lexicon', 'Default Lexicon',
+                                  (wordsplitter, casenormalizer, stopwords))
+        
+        indexes = item.indexes()
+        schema = item.schema()
+        
+        if 'indexable_content' not in indexes:
+            zctextindex_extras = Record()
+            zctextindex_extras.index_type = 'Okapi BM25 Rank'
+            zctextindex_extras.lexicon_id = 'Lexicon'
+            # add the full text index 
+            zctextindex_extras.doc_attr = 'indexable_content'
+        
+            item.addIndex('indexable_content',
+                             'ZCTextIndex', zctextindex_extras)
+        
+        for index_id, index_type in (('id', 'FieldIndex'),
+                                     ('meta_type', 'FieldIndex'),
+                                     ('modification_time', 'DateIndex'),
+                                     ('resource_locator', 'FieldIndex')):
+            if index_id not in indexes:
+                item.addIndex(index_id, index_type)
+            
+        # store the common metadata for sorting
+        for md in ('id', 'meta_type', 'title', 'size',
+                   'modification_time', 'dc_creator',
+                   'indexable_summary'):
+            if md not in schema:
+                item.addColumn(md)
+                
+        return True
+        
+# Zope Management Methods
+#
+manage_addXWFCatalogForm = PageTemplateFile(
+   'management/manage_addXWFCatalogForm.zpt',
+    globals(), __name__='manage_addXWFCatalogForm')
+
+def manage_addXWFCatalog(self, id, REQUEST=None, RESPONSE=None, submit=None):
+    """ Add a new instance of XWFCatalog.
+        
+    """
+    obj = XWFCatalog(id)
+    self._setObject(id, obj)
+    
+    if RESPONSE and submit:
+        if submit.strip().lower() == 'add':
+            RESPONSE.redirect('%s/manage_main' % self.DestinationURL())
+        else:
+            RESPONSE.redirect('%s/manage_main' % id)
+
+def initialize(context):
+    context.registerClass(
+        XWFCatalog,
+        permission='Add XWF News',
+        constructors=(manage_addXWFCatalogForm,
+                      manage_addXWFCatalog),
+        icon='icons/ic-xwfcatalog.png'
+        )
+        
