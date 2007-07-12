a2b4d56f9e9ce1bb2efec8fb1de105971fd0dd5c
Richard Waid <richard@iopen.net>
2007-07-12 11:41:16 +0000

    Added an expiry interval enabled cache type
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFCore/trunk%402256

--- cache.py
+++ cache.py
@@ -6,12 +6,22 @@
 from zope.interface.declarations import implements
 from zope.interface.interface import Interface
 import ThreadLock
+import datetime
 
 class ICache(Interface):
     def set_max_objects(max):
         """ Set the maximum number of objects that the cache may contain.
         
         This may not be implemented by some classes, since not all caches
+        may want to implement expiry based on this parameter.
+        
+        """
+
+    def set_expiry_interval(self, dt):
+        """ Set the expiry interval of the cache, passing in a datetime.timedelta
+        object.
+
+        This may not be implemented by some classes, since not all classes
         may want to implement expiry based on this parameter.
         
         """
@@ -42,6 +52,9 @@ class SimpleCache:
         
     def set_max_objects(self, max):
         raise NotImplementedError
+
+    def set_expiry_interval(self, dt):
+        raise NotImplementedError
         
     def add(self, key, object):
         try:
@@ -57,6 +70,51 @@ class SimpleCache:
         
     def get(self, key):
         return self.cache.get(key, None)
+import logging
+logger = logging.getLogger()
+
+class SimpleCacheWithExpiry:
+    """ Implement ICache with expiry.
+    
+    """
+    implements(ICache)
+    __thread_lock = ThreadLock.allocate_lock()
+    def __init__(self):
+        self.cache = {}
+        self.expiry_interval = None
+                
+    def set_max_objects(self, max):
+        raise NotImplementedError
+
+    def set_expiry_interval(self, dt):
+        if not isinstance(dt, datetime.timedelta):
+            raise TypeError('expiry interval must be an instance of datetime.timedelta')
+        
+        self.expiry_interval = dt
+        
+    def add(self, key, object):
+        try:
+            self.__thread_lock.acquire()
+            self.cache[key] = (datetime.datetime.now()+self.expiry_interval,
+                               object)
+        finally:
+            self.__thread_lock.release()
+        
+        return True
+    
+    def has_key(self, key):
+        return self.cache.has_key(key)
+        
+    def get(self, key):
+        cache_object = self.cache.get(key, None)
+        retval = None
+        if cache_object and datetime.datetime.now() < cache_object[0]:
+            logging.info('cache expires in %s' % (cache_object[0]-datetime.datetime.now()))
+            retval = cache_object[1]
+        elif cache_object:
+            del(self.cache[key])
+            
+        return retval
 
 class LRUCache:
     """ Implements a ICache based on a Least Recently Used mechanism.
@@ -71,6 +129,9 @@ class LRUCache:
         
     def set_max_objects(self, size):
         self.cache_size = size
+
+    def set_expiry_interval(self, dt):
+        raise NotImplementedError
         
     def __do_add(self, key, object):
         try:
